1132e2d4582614ade1a853c7ae14cb47c4ae941d
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-25 20:56:06 +0100

    adicionando no kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,8 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'admin_auth' => \App\Http\Middleware\Administrador::class,
+        'user_auth' =>\App\Http\Middleware\User::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
