ad8f38addfe7e8da35105b74b2ac39eee2231893
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-25 18:24:52 +0100

    criando as rotas principais

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', "HomeController@index")->name('home');
+
+Route::get('/login', "UsuarioController@login")->name('login');
+Route::get('/logout', "UsuarioController@logout")->name('logout');
+Route::post('/logar', "UsuarioController@logar")->name('logar');
