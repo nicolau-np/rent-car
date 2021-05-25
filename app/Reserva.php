768b17656d0f1a0d8d2291bd9ef4eb50f827a329
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-25 20:04:37 +0100

    trabalhando no mmodelo reserva

--- app/Reserva.php
+++ app/Reserva.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reserva extends Model
 {
-    //
+    protected $table = "reservas";
+
+    protected $fillable = [
+        'id_cliente',
+        'id_automovel',
+        'data_requisicao',
+        'hora_requisicao',
+        'local_receber',
+        'estado',
+    ];
+
+    public function cliente(){
+        return $this->belongsTo(Cliente::class, 'id_cliente', 'id');
+    }
+
+    public function automovel(){
+        return $this->belongsTo(Automovel::class, 'id_automovel', 'id');
+    }
 }
