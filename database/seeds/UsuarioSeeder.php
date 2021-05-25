bf39c781ea58b9c0352ecad65992cf6254c431ae
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-25 20:13:54 +0100

    trabalhando no usuarioSeeder

--- database/seeds/UsuarioSeeder.php
+++ database/seeds/UsuarioSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsuarioSeeder extends Seeder
 {
@@ -9,8 +10,26 @@ class UsuarioSeeder extends Seeder
      *
      * @return void
      */
+
+
     public function run()
     {
-        //
+        DB::table('usuarios')->insert(
+            array(
+                [
+                    'id_pessoa'=>1,
+                    'email'=>"tarcisotc@gmail.com",
+                    'acesso'=>"user",
+                    'palavra_passe'=>md5("tarcisotc"),
+                    'estado'=>"on",
+                ],[
+                    'id_pessoa'=>2,
+                    'email'=>"arafate@gmail.com",
+                    'acesso'=>"admin",
+                    'palavra_passe'=>md5("arafate"),
+                    'estado'=>"on",
+                ],
+            )
+        );
     }
 }
