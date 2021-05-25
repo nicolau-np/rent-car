6b1a40306231a99fdfe133eeb0b7907b2e01e58c
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-25 18:34:36 +0100

    adicionando pacotes de css e jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" name="viewport">
-  <title>Dashboard &mdash; Stisla</title>
+  <title>{{$title}}</title>
 
   <link rel="stylesheet" href="../dist/modules/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="../dist/modules/ionicons/css/ionicons.min.css">
