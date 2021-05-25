4bac11096335bf2e92276d7608ebe92dcdda3a28
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-25 18:27:08 +0100

    criando as views principais

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index(){
+        $data = [
+            'title'=>"Renta Car",
+            'menu'=>"Home",
+            "submenu"=> null,
+            'type'=>"home",
+        ];
+
+        return view('home');
+    }
 }
