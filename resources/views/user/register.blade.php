afa9137476f1559786ebf4a91134786294aa644c
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-25 19:30:32 +0100

    update register view

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -14,33 +14,53 @@
               <div class="card-header"><h4>Registro</h4></div>
 
               <div class="card-body">
-                <form method="POST">
+                @if (session('error'))
+                    <div class="alert bg-danger" role="alert"><em class="fa fa-lg fa-warning">&nbsp;</em> {{__(session('error'))}} <a href="#" class="pull-right"><em class="fa fa-lg fa-close"></em></a></div>
+                @endif
+
+                @if (session('success'))
+                    <div class="alert bg-success" role="alert"><em class="fa fa-lg fa-check">&nbsp;</em> {{session('success')}} <a href="#" class="pull-right"><em class="fa fa-lg fa-close"></em></a></div>
+                @endif
+                {{Form::open(['method'=>"post", 'name'=>"formLogin", 'url'=>"/regiter_store"])}}
                   <div class="row">
                     <div class="form-group col-6">
                       <label for="frist_name">Nome </label>
-                      <input id="frist_name" type="text" class="form-control" name="frist_name" autofocus>
+                      <input id="frist_name" type="text" class="form-control" name="nome" autofocus>
+                      @if($errors->has('nome'))
+                      <span class="text-danger">{{$errors->first('nome')}}</span>
+                      @endif
                     </div>
                     <div class="form-group col-6">
                       <label for="last_name">Sobrenome</label>
-                      <input id="last_name" type="text" class="form-control" name="last_name">
+                      <input id="last_name" type="text" class="form-control" name="sobrenome">
+                      @if($errors->has('sobrenome'))
+                      <span class="text-danger">{{$errors->first('sobrenome')}}</span>
+                      @endif
                     </div>
                   </div>
 
                   <div class="form-group">
                     <label for="email">Email</label>
                     <input id="email" type="email" class="form-control" name="email">
-                    <div class="invalid-feedback">
-                    </div>
+                    @if($errors->has('email'))
+                      <span class="text-danger">{{$errors->first('email')}}</span>
+                    @endif
                   </div>
 
                   <div class="row">
                     <div class="form-group col-6">
                       <label for="password" class="d-block">Palavra-Passe</label>
-                      <input id="password" type="password" class="form-control" name="password">
+                      <input id="password" type="password" class="form-control" name="palavra_passe">
+                      @if($errors->has('palavra_passe'))
+                      <span class="text-danger">{{$errors->first('palavra_passe')}}</span>
+                    @endif
                     </div>
                     <div class="form-group col-6">
                       <label for="password2" class="d-block">Confirm. Palavra-Passe</label>
-                      <input id="password2" type="password" class="form-control" name="password-confirm">
+                      <input id="password2" type="password" class="form-control" name="password_confirm">
+                      @if($errors->has('password_confirm'))
+                      <span class="text-danger">{{$errors->first('password_confirm')}}</span>
+                    @endif
                     </div>
                   </div>
 
@@ -58,7 +78,7 @@
                       Registrar
                     </button>
                   </div>
-                </form>
+                {{Form::close()}}
               </div>
             </div>
             <div class="simple-footer">
